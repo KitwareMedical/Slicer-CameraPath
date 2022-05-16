6e56cfcc4d7c7b399c9013d6fd567a189d0b2eda
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-05-16 12:28:08 -0400

    COMP: Update script to fix "TabError: inconsistent use of tabs and spaces in indentation"

--- Modules/Scripted/testCameraPath/testCameraPath.py
+++ Modules/Scripted/testCameraPath/testCameraPath.py
@@ -1,5 +1,13 @@
-import vtk, qt, ctk, slicer, time
+import time
 import os
+
+import ctk
+import qt
+import vtk
+
+import slicer
+
+
 #
 # testCameraPath
 #
@@ -17,6 +25,7 @@ class testCameraPath:
         self.parent = parent
 
 
+#
 # qtestCameraPathWidget
 #
 
@@ -50,133 +59,106 @@ class testCameraPathWidget:
         self.RemoveButton.toolTip = "Remove KeyFrames"
         self.RemoveButton.connect('clicked()', self.onRemoveButton)
         progress_layout.addWidget(self.RemoveButton)
-        
+
         self.AddButton = qt.QPushButton("Add KeyFrame")
         self.AddButton.toolTip = "Add KeyFrame"
         self.AddButton.connect('clicked()', self.onAddButton)
         progress_layout.addWidget(self.AddButton)
-        
+
         self.CreateButton = qt.QPushButton("Create Path")
         self.CreateButton.toolTip = "Create Path"
         self.CreateButton.connect('clicked()', self.onCreateButton)
         progress_layout.addWidget(self.CreateButton)
-        
+
         self.FlyButton = qt.QPushButton("Fly !")
         self.FlyButton.toolTip = "Fly !"
         self.FlyButton.connect('clicked()', self.onFlyButton)
         self.FlyButton.setEnabled(False)
         progress_layout.addWidget(self.FlyButton)
-        
+
         # LOGIC
         self.defaultCam = slicer.mrmlScene.GetNodeByID('vtkMRMLCameraNode1')
         self.Time = 0
         self.CameraList = []
         self.cameraPath = slicer.vtkMRMLCameraPathNode()
-	posSpline = self.cameraPath.GetPositionSplines()
-	focalSpline = self.cameraPath.GetFocalPointSplines()
-	viewSpline = self.cameraPath.GetViewUpSplines()
-	
-	posSpline.SetName('posSpline')
-	focalSpline.SetName('focalSpline')
-	viewSpline.SetName('viewSpline')
-	
-	slicer.mrmlScene.AddNode(self.cameraPath)
-	slicer.mrmlScene.AddNode(posSpline)
-	slicer.mrmlScene.AddNode(focalSpline)
-	slicer.mrmlScene.AddNode(viewSpline)
-	
-	self.Flying = False
+        posSpline = self.cameraPath.GetPositionSplines()
+        focalSpline = self.cameraPath.GetFocalPointSplines()
+        viewSpline = self.cameraPath.GetViewUpSplines()
+
+        posSpline.SetName('posSpline')
+        focalSpline.SetName('focalSpline')
+        viewSpline.SetName('viewSpline')
+
+        slicer.mrmlScene.AddNode(self.cameraPath)
+        slicer.mrmlScene.AddNode(posSpline)
+        slicer.mrmlScene.AddNode(focalSpline)
+        slicer.mrmlScene.AddNode(viewSpline)
+
+        self.Flying = False
 
     def onRemoveButton(self):
-      
        print "-- Removing KeyFrames"
        self.cameraPath.RemoveKeyFrames()
-       
+
        if self.CameraList:
-	   for camera in self.CameraList:
-	       print camera.GetName()
-	       slicer.mrmlScene.RemoveNode(camera)
-	   del self.CameraList[:]
-	   
+           for camera in self.CameraList:
+               print camera.GetName()
+               slicer.mrmlScene.RemoveNode(camera)
+           del self.CameraList[:]
+
        self.Time = 0
-      
+
     def onAddButton(self):
-      
-	print " -- Adding Keyframe at t =",self.Time
-	camera = slicer.mrmlScene.CreateNodeByClass('vtkMRMLCameraNode')
-	slicer.mrmlScene.AddNode(camera)
-	
-	x = vtk.mutable(0)
-	y = vtk.mutable(0)
-	z = vtk.mutable(0)
-	value = [x,y,z]
-	
-	self.defaultCam.GetPosition(value)
-	camera.SetPosition(value)
-	self.defaultCam.GetFocalPoint(value)
-	camera.SetFocalPoint(value)
-	self.defaultCam.GetViewUp(value)
-	camera.SetViewUp(value)
-	
-	cameraName = 'Camera T = '+str(self.Time)
-	print cameraName
-	camera.SetName(cameraName)
-	
-	self.cameraPath.AddKeyFrame(self.Time, camera)
-	self.Time += 100
-	self.CameraList.append(camera)
-	
+        print " -- Adding Keyframe at t =",self.Time
+        camera = slicer.mrmlScene.CreateNodeByClass('vtkMRMLCameraNode')
+        slicer.mrmlScene.AddNode(camera)
+
+        x = vtk.mutable(0)
+        y = vtk.mutable(0)
+        z = vtk.mutable(0)
+        value = [x,y,z]
+
+        self.defaultCam.GetPosition(value)
+        camera.SetPosition(value)
+        self.defaultCam.GetFocalPoint(value)
+        camera.SetFocalPoint(value)
+        self.defaultCam.GetViewUp(value)
+        camera.SetViewUp(value)
+
+        cameraName = 'Camera T = '+str(self.Time)
+        print cameraName
+        camera.SetName(cameraName)
+
+        self.cameraPath.AddKeyFrame(self.Time, camera)
+        self.Time += 100
+        self.CameraList.append(camera)
+
         self.FlyButton.setEnabled(False)
 
     def onCreateButton(self):
-
-	print " -- Creating Path"
-	self.cameraPath.CreatePath();
+        print " -- Creating Path"
+        self.cameraPath.CreatePath();
         self.FlyButton.setEnabled(True)
-
 
     def onFlyButton(self):
-      
         if not self.Flying:
-	    print " -- FLYING !"
-	    self.Flying = True
+            print " -- FLYING !"
+            self.Flying = True
             self.FlyButton.setText("Stop")
-        
-	    for t in range(0,self.Time-99):
-	        if not self.Flying:
-		  return
-		self.cameraPath.GetCameraAt(t,self.defaultCam)
-		self.defaultCam.ResetClippingRange()
-		print t
-		slicer.app.processEvents()
-		time.sleep(0.05)
-		
-	    print "End"
-	    self.Flying = False
-	    self.FlyButton.setText("Fly!")
-	
-	else:
-	    print " -- Stopped"
-	    self.Flying = False
-	    self.FlyButton.setText("Fly!")
-	
+            for t in range(0,self.Time-99):
+                if not self.Flying:
+                    return
+                self.cameraPath.GetCameraAt(t,self.defaultCam)
+                self.defaultCam.ResetClippingRange()
+                print t
+                slicer.app.processEvents()
+                time.sleep(0.05)
+            print "End"
+            self.Flying = False
+            self.FlyButton.setText("Fly!")
+        else:
+            print " -- Stopped"
+            self.Flying = False
+            self.FlyButton.setText("Fly!")
         self.FlyButton.setEnabled(True)
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
+
