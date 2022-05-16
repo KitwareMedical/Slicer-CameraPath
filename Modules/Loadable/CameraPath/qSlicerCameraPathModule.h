5feaad95a5437162bc4244b370f143927bf2f16f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-05-16 12:35:42 -0400

    BUG: Fix loading of module when built against Slicer 5.x and Qt 5.15
    This commit fixes the following runtime error:
    
      Failed to extract plugin meta data from '/path/to/lib/Slicer-5.1/qt-loadable-modules/libqSlicerCameraPathModule.so'
    
    References:
    * https://www.slicer.org/wiki/Documentation/Nightly/Developers/Tutorials/MigrationGuide#Qt5:_Update_loadable_modules_to_use_new_plugin_macros
    * https://wiki.qt.io/Transition_from_Qt_4.x_to_Qt5#Plugin_loading

--- Modules/Loadable/CameraPath/qSlicerCameraPathModule.h
+++ Modules/Loadable/CameraPath/qSlicerCameraPathModule.h
@@ -31,6 +31,7 @@ qSlicerCameraPathModule
   : public qSlicerLoadableModule
 {
   Q_OBJECT
+  Q_PLUGIN_METADATA(IID "org.slicer.modules.loadable.qSlicerLoadableModule/1.0");
   Q_INTERFACES(qSlicerLoadableModule);
 
 public:
